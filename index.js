80a28f39cfdb8117a424a9bde1e0c51fe92a8a58
Miromar Junior <miromarjunior@gmail.com>
2022-02-07 22:45:40 -0300

    Em andamento

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Ola tudo ok")
+const express = require("express");
+const bodyParse = require("body-parser");
+const userRoute = require("./routers/userRoute");
+const res = require("express/lib/response");
+
+const app = express();
+app.use(bodyParse.urlencoded({extended : false}));
+const port = 3000;
+
+userRoute(app);
+
+/**app.get("/", (res, req) => res.send("Ola mundo express"));*/
+
+app.listen(port, ()=> console.log("API rodando Porta 3000"));
